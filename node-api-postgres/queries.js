758cb4f69baea7484525b4ef1648150ee27981f0
derekhuggens <derekhuggens@gmail.com>
2022-03-18 17:13:47 -0400

    adding necessary node & express files

---
+++ node-api-postgres/queries.js
@@ -0,0 +1,35 @@
+//document.writeln("script type='text/javascript", src='config.js');
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'insurance_fraud_db',
+  password: 'insertpassword',
+  port: 5432,
+})
+
+const getColumns = (request, response) => {
+    pool.query('SELECT age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable FROM insurance_claims', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+const createNewRow = (request, response) => {
+    const { age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable } = request.body
+  
+    pool.query('INSERT INTO insurance_claims (age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable) VALUES ($1, $2, $3, $4, $5, $6)', [age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`User added: ${result.insertRows}`)
+    })
+  }
+
+module.exports = {
+    getColumns,
+    createNewRow,
+}
