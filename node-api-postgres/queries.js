8c71de30c124e4b4be2e71bf4ad8749bfdb38f2b
derekhuggens <derekhuggens@gmail.com>
2022-03-23 13:37:04 -0400

    adding node and express files

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -5,12 +5,12 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'insurance_fraud_db',
-  password: 'insertpassword',
+  password: 'Jackyboy89!',
   port: 5432,
 })
 
 const getColumns = (request, response) => {
-    pool.query('SELECT age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable FROM insurance_claims', (error, results) => {
+    pool.query('SELECT age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible FROM insurance_claims', (error, results) => {
       if (error) {
         throw error
       }
@@ -19,13 +19,13 @@ const getColumns = (request, response) => {
   }
 
 const createNewRow = (request, response) => {
-    const { age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable } = request.body
+    const { age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible } = request.body
   
-    pool.query('INSERT INTO insurance_claims (age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable) VALUES ($1, $2, $3, $4, $5, $6)', [age, months_as_customer, incident_state, auto_make, auto_model, policy_deductable], (error, results) => {
+    pool.query('INSERT INTO insurance_claims (age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible) VALUES ($1, $2, $3, $4, $5, $6)', [age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible], (error, results) => {
       if (error) {
         throw error
       }
-      response.status(201).send(`User added: ${result.insertRows}`)
+      response.status(201).send(`User added: `)
     })
   }
 
