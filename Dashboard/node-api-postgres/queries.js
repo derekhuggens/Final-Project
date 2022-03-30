0e0a362abb852e26264a4e45d1a56da82487f928
Derek Huggens <derekhuggens@gmail.com>
2022-03-30 17:43:46 -0400

    Form to JSON functionality

--- Dashboard/node-api-postgres/queries.js
+++ Dashboard/node-api-postgres/queries.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 });
 
 const getColumns = (request, response) => {
-    pool.query('SELECT age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible FROM insurance_claims', (error, results) => {
+    pool.query('SELECT insured_sex, age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible FROM insurance_claims', (error, results) => {
       if (error) {
         throw error
       }
@@ -18,9 +18,9 @@ const getColumns = (request, response) => {
   }
 
 const createNewRow = (request, response) => {
-    const { age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible } = request.body
+    const {insured_sex, age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible } = request.body
   
-    pool.query('INSERT INTO insurance_claims (age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible) VALUES ($1, $2, $3, $4, $5, $6)', [age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible], (error, results) => {
+    pool.query('INSERT INTO insurance_claims (insured_sex, age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible) VALUES ($1, $2, $3, $4, $5, $6, $7)', [insured_sex, age, months_as_customer, incident_state, auto_make, auto_model, policy_deductible], (error, results) => {
       if (error) {
         throw error
       }
