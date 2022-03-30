e56e242c8dc16272b67495a70a3fd90bd5f2eb32
Derek Huggens <derekhuggens@gmail.com>
2022-03-30 16:48:32 -0400

    Form functionality

--- Dashboard/node-api-postgres/index.js
+++ Dashboard/node-api-postgres/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pug = require('pug');
 const app = express();
 const db = require('./queries');
 const port = 3001;
@@ -16,7 +15,7 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
 
-app.get('/rows', db.getColumns))
+app.get('/rows', db.getColumns)
 app.post('/rows', db.createNewRow)
 
 app.listen(port, () => {
